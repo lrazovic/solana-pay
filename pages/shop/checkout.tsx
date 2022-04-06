e6431a04d88a0d9096412831352321c6ee2f93ff
Leonardo Razovic <4128940+lrazovic@users.noreply.github.com>
2022-04-06 10:19:49 +0200

    Cleanup and formatting

--- pages/shop/checkout.tsx
+++ pages/shop/checkout.tsx
@@ -1,91 +1,96 @@
-// pages/shop/checkout.tsx
-import { createQR, encodeURL, EncodeURLComponents, findTransactionSignature, FindTransactionSignatureError, validateTransactionSignature, ValidateTransactionSignatureError } from "@solana/pay";
+import { createQR, findTransactionSignature, FindTransactionSignatureError, validateTransactionSignature, ValidateTransactionSignatureError } from "@solana/pay";
 import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
 import { clusterApiUrl, Connection, Keypair } from "@solana/web3.js";
-import BigNumber from "bignumber.js";
-import { time } from "console";
 import { useRouter } from "next/router";
 import { useEffect, useMemo, useRef } from "react";
 import BackLink from "../../components/BackLink";
 import PageHeading from "../../components/PageHeading";
-import { shopAddress, usdcAddress } from "../../lib/addresses";
-import calculatePrice from "../../lib/calculatePrice";
+import { useConfig } from "../../contexts/ConfigProvider";
 
 export default function Checkout() {
-    const router = useRouter()
+  const router = useRouter()
 
-    // ref to a div where we'll show the QR code
-    const qrRef = useRef<HTMLDivElement>(null)
+  // ref to a div where we'll show the QR code
+  const qrRef = useRef<HTMLDivElement>(null)
 
-    const amount = useMemo(() => calculatePrice(router.query), [router.query])
-
-    // Unique address that we can listen for payments to
-    const reference = useMemo(() => Keypair.generate().publicKey, [])
-
-    // Get a connection to Solana devnet
-    const network = WalletAdapterNetwork.Devnet
-    const endpoint = clusterApiUrl(network)
-    const connection = new Connection(endpoint)
-
-
-    // Solana Pay transfer params
-    const urlParams: EncodeURLComponents = {
-        recipient: shopAddress,
-        splToken: usdcAddress,
-        amount,
-        reference,
-        label: "Cookies Inc",
-        message: "Thanks for your order! 🍪",
+  // Read the URL query (which includes our chosen products)
+  const searchParams = new URLSearchParams();
+  for (const [key, value] of Object.entries(router.query)) {
+    if (value) {
+      if (Array.isArray(value)) {
+        for (const v of value) {
+          searchParams.append(key, v);
+        }
+      } else {
+        searchParams.append(key, value);
+      }
     }
+  }
 
-    // Encode the params into the format shown
-    const url = encodeURL(urlParams)
-    console.log({ url })
+  // Generate the unique reference which will be used for this transaction
+  const reference = useMemo(() => Keypair.generate().publicKey, []);
 
-    // Show the QR code
-    useEffect(() => {
-        const qr = createQR(url, 512, 'transparent')
-        if (qrRef.current && amount.isGreaterThan(0)) {
-            qrRef.current.innerHTML = ''
-            qr.append(qrRef.current)
+  // Add it to the params we'll pass to the API
+  searchParams.append('reference', reference.toString());
+
+  console.log({ query: router.query, params: searchParams.toString() })
+
+  // Get a connection to Solana devnet
+  const network = WalletAdapterNetwork.Devnet
+  const endpoint = clusterApiUrl(network)
+  const connection = new Connection(endpoint)
+
+  const { baseUrl } = useConfig()
+
+  // Encode the params into the format shown
+  const apiUrlEncoded = encodeURIComponent(`${baseUrl}/api/makeTransaction?${searchParams.toString()}`)
+  const url = `solana:${apiUrlEncoded}`
+  console.log({ url })
+
+  // Show the QR code
+  useEffect(() => {
+    const qr = createQR(url, 512, 'transparent')
+    if (qrRef.current) {
+      qrRef.current.innerHTML = ''
+      qr.append(qrRef.current)
+    }
+  }, [url])
+
+  // Check every 0.5s if the transaction is completed
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        // Check if there is any transaction for the reference
+        await findTransactionSignature(connection, reference, {}, 'confirmed')
+        // Validate that the transaction has the expected recipient, amount and SPL token
+        // await validateTransactionSignature(connection, signatureInfo.signature, shopAddress, amount, usdcAddress, reference, 'confirmed')
+        router.push('/shop/confirmed')
+      } catch (e) {
+        if (e instanceof FindTransactionSignatureError) {
+          // No transaction found yet, ignore this error
+          return;
         }
-    })
-
-    // Check every 0.5s if the transaction is completed
-    useEffect(() => {
-        const interval = setInterval(async () => {
-            try {
-                // Check if there is any transaction for the reference
-                const signatureInfo = await findTransactionSignature(connection, reference, {}, 'confirmed')
-                // Validate that the transaction has the expected recipient, amount and SPL token
-                await validateTransactionSignature(connection, signatureInfo.signature, shopAddress, amount, usdcAddress, reference, 'confirmed')
-                router.push('/shop/confirmed')
-            } catch (e) {
-                if (e instanceof FindTransactionSignatureError) {
-                    // No transaction found yet, ignore this error
-                    return;
-                }
-                if (e instanceof ValidateTransactionSignatureError) {
-                    // Transaction is invalid
-                    console.error('Transaction is invalid', e)
-                    return;
-                }
-                console.error('Unknown error', e)
-            }
-        }, 500)
-        return () => {
-            clearInterval(interval)
+        if (e instanceof ValidateTransactionSignatureError) {
+          // Transaction is invalid
+          console.error('Transaction is invalid', e)
+          return;
         }
-    }, [])
+        console.error('Unknown error', e)
+      }
+    }, 500)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [])
 
-    return (
-        <div className="flex flex-col gap-8 items-center">
-            <BackLink href='/shop'>Cancel</BackLink>
+  return (
+    <div className="flex flex-col gap-8 items-center">
+      <BackLink href='/shop'>Cancel</BackLink>
 
-            <PageHeading>Checkout ${amount.toString()}</PageHeading>
+      <PageHeading>Checkout</PageHeading>
 
-            {/* div added to display the QR code */}
-            <div ref={qrRef} />
-        </div>
-    )
+      {/* div added to display the QR code */}
+      <div ref={qrRef} />
+    </div>
+  )
 }
--- pages/shop/confirmed.tsx
+++ pages/shop/confirmed.tsx
@@ -4,13 +4,13 @@ import Confirmed from '../../components/Confirmed';
 import PageHeading from '../../components/PageHeading';
 
 export default function ConfirmedPage() {
-    return (
-        <div className='flex flex-col gap-8 items-center'>
-            <BackLink href='/shop'>Next order</BackLink>
+  return (
+    <div className='flex flex-col gap-8 items-center'>
+      <BackLink href='/shop'>Next order</BackLink>
 
-            <PageHeading>Thankyou, enjoy your cookies!</PageHeading>
+      <PageHeading>Thankyou, enjoy your cookies!</PageHeading>
 
-            <div className='h-80 w-80'><Confirmed /></div>
-        </div>
-    )
+      <div className='h-80 w-80'><Confirmed /></div>
+    </div>
+  )
 }
